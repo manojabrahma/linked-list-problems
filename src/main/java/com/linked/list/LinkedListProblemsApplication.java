591ae8a25a31a8c58c5194e0db46f5419353c068
Manoja Brahma <manoja.brahma@trellix.com>
2023-06-14 16:45:20 +0530

    adding templatecode

---
+++ src/main/java/com/linked/list/LinkedListProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.linked.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedListProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedListProblemsApplication.class, args);
+	}
+
+}
